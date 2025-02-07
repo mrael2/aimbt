e506e80ec3a8ef0f0ccd26484464f41c820781ef
Michael C. Rael <mcrael@adams.edu>
2025-02-07 11:19:36 -0700

    Parse the file coming through stdin.

--- src/main.zig
+++ src/main.zig
@@ -7,4 +7,8 @@ pub fn main() !void {
     var general_purpose_allocator: std.heap.GeneralPurposeAllocator(.{}) = .init;
     const gpa = general_purpose_allocator.allocator();
     const text = try stdin.readAllAlloc(gpa, 5_000_000);
+
+    _ = try std.json.parseFromSlice(std.json.Value, gpa, text, .{});
+
+    try stdout.print("hello\n", .{});
 }
